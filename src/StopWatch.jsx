609688bbe238b87082b5ec654844f7414ed86144
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2024-12-02 11:47:49 +0530

    logic for application

--- src/StopWatch.jsx
+++ src/StopWatch.jsx
@@ -41,6 +41,7 @@ function StopWatch(){
         let millisec=Math.floor((elapsedTime%1000)/10);
 
 
+        // eslint-disable-next-line no-unused-vars
         hour=String(hour).padStart(2,"0");
         min=String(min).padStart(2,"0");
         sec=String(sec).padStart(2,"0");
